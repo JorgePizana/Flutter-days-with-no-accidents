d71ca0af1714a68dd295057f1a528afcecfdd2af
Jorge Pizaña Mtz <45296571+JorgePizana@users.noreply.github.com>
2021-05-17 16:23:03 -0600

    Added Timer classes
    -- Added corresponding constructor class that allows to format the time as days:hours:minutes:seconds
    -- constructTime has a different equation for each type of time
    --initState gets current time and starts timer
    -- cancelTimer ands dispose cancels the timer and sets it to null

--- flutter_app_days_without_accs/lib/screens/homePage/index.dart
+++ flutter_app_days_without_accs/lib/screens/homePage/index.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../routes.dart';
@@ -7,13 +8,79 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime _lastButtonPressed;
+  Timer _ticker;
+  int seconds;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Testing'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Days without accidents"),
+          Text(constructTime(seconds)),
+        ],
+      ),
     );
-
   }
+  //time format,Convert to the corresponding hh:mm:ss format according to the total seconds
+  String constructTime(int seconds){
+    int days = seconds ~/86400;
+    int hour = seconds ~/3600;
+    int minute = seconds%3600 ~/60;
+    int second = seconds%60;
+    return formatTime (days) + ":" + formatTime (hour) + ":" + formatTime (minute) + ":" + formatTime (second);
+  }
+  //number formatting,Convert 0 ~ 9 time to 00 ~ 09
+  String formatTime (int timeNum){
+    return timeNum < 10? "0" + timeNum.toString():timeNum.toString();
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    //Get the current time
+    var now = DateTime.now();
+    //Get 2 minute interval
+    var twoHours = now.add(Duration(minutes:0)).difference(now);
+    //for countdouwn Get the total number of seconds, 120 secons in 2 minutes
+    //when counter minutes:0
+
+    seconds = twoHours.inSeconds;
+    startTimer();
+  }
+
+  void startTimer(){
+    //set callback once every 1 seconds
+    const period = const Duration(seconds: 1);
+    _ticker = Timer.periodic(period, (timer) {
+      //update interface
+      setState(() {
+        //seconds +1, because once every second
+        seconds++;
+      });
+      //if (seconds == 0){
+        //When is countdown seconds--, cancel time
+        //cancelTimer();
+      //}
+    });
+  }
+
+  void cancelTimer(){
+    if (_ticker!=null){
+      _ticker.cancel();
+      _ticker=null;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cancelTimer();
+  }
+
+
 
 }
--- flutter_app_days_without_accs/lib/styles.dart
+++ flutter_app_days_without_accs/lib/styles.dart
@@ -4,13 +4,58 @@ class ThemeBuilder {
   ThemeBuilder._();
 
   static final ThemeData lightTheme = ThemeData(
-      scaffoldBackgroundColor: Colors.white,
-      backgroundColor: Colors.white,
-
+    scaffoldBackgroundColor: Colors.white,
+    backgroundColor: Colors.white,
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        //Appbar titles
+        color: Colors.black,
+        fontFamily: 'Montserrat',
+      ),
+      headline5: TextStyle(
+          //Big title
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Montserrat',
+          fontSize: 20),
+      headline4: TextStyle(
+          //Big Title in red/orange
+          color: Color.fromARGB(255, 201, 73, 85),
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Montserrat',
+          fontSize: 20),
+      headline3: TextStyle(
+        //For Text fields
+        fontFamily: 'Montserrat',
+        color: Colors.grey[800],
+        fontSize: 15,
+      ),
+      headline2: TextStyle(
+        //Home screen text
+        fontFamily: 'Montserrat',
+        color: Colors.black,
+        fontSize: 20,
+      ),
+    ),
+    cardTheme: CardTheme(
+      //Card theme
+      color: Colors.white,
+      shadowColor: Colors.grey[400].withOpacity(.6),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.grey,
+    ),
+    tabBarTheme: TabBarTheme(
+      //Possibly For under bar
+      labelColor: Color.fromARGB(255, 201, 73, 85),
+      unselectedLabelColor: Colors.grey[400],
+    ),
+    buttonColor: Color.fromARGB(255, 201, 73, 85),
   );
+
+
   static final ThemeData darkTheme = ThemeData(
     scaffoldBackgroundColor: Colors.black,
     backgroundColor: Colors.grey,
   );
-
 }
